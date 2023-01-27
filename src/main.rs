779ce39076df0ab6323bb89919007707efc36025
lucas guillemette <36134032+lgilmet@users.noreply.github.com>
2023-01-27 13:39:34 -0500

    First tuto

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports)] // TODO: Remove this line when you're done. This is just to suppress warnings for unused imports in the console.
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
+fn main() {
+
+    // FIRST EXAMPLE
+    // println!("What is your name?");
+    // let mut name = String::new();
+    // let greeting = "Nice to meet you";
+    // io::stdin()
+    //     .read_line(&mut name)
+    //     .expect("Failed to read line");
+    // println!("Hello, {}! {}", name.trim_end(), greeting);
+}
